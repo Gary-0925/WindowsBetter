3486d7e9a3798167ccff12c3055a467244893031
Gary0 <Gary20120925@163.com>
2026-02-08 19:14:29 +0800

    Delete cmpl.bat

--- cmpl.bat
+++
@@ -1,4 +0,0 @@
-g++ -c res\watermark.cpp -o o\watermark.o
-g++ res\monitor.cpp -o WindowsBetter\Wmonitor.exe -luser32 -lgdi32 -lkernel32
-windres res\winbetter.rc o\winbetter.o
-g++ o\watermark.o o\winbetter.o -o WindowsBetter\WindowsBetter.exe -luser32 -lgdi32 -lkernel32
